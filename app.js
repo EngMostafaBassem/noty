57063d9f43dbef12328cf190bfed376e7de7adfe
vip <vip@DESKTOP-TVLU5IH>
2021-06-02 20:26:31 +0200

    'commit2'

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express=require("express")
 const path=require("path")
 const mongoose=require("mongoose")
-const userRoutes=require('./Routes/user.Routes')
-const noteRoutes=require("./Routes/note.Routes")
+const userRoutes=require('./routes/user.Routes')
+const noteRoutes=require("./routes/note.Routes")
 var session = require('express-session')
 const app=express()
 // initial setup
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname,"Public")))
+app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
-// MongoDB setup
-
-mongoose.connect('mongodb+srv://dbfcis:j4MQgkB76feFW7y@cluster0.a5rkl.mongodb.net/NoteDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 //setup Session
@@ -23,8 +20,8 @@ app.use(session({
    
   }))
 //setup Routes
-
 app.use(userRoutes)
 app.use(noteRoutes)
-
+// MongoDB setup
+mongoose.connect('mongodb+srv://dbfcis:j4MQgkB76feFW7y@cluster0.a5rkl.mongodb.net/NoteDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 app.listen(process.env.PORT||3000)
