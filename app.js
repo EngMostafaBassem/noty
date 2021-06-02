c98a13bebfac29336e29f99616fcbabe5585cea0
vip <vip@DESKTOP-TVLU5IH>
2021-06-02 21:35:02 +0200

    '33'

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const path=require("path")
 const mongoose=require("mongoose")
-//const userRoutes=require('./routes/user.Routes')
-//const noteRoutes=require("./routes/note.Routes")
+const userRoutes=require('./routes/user')
+const noteRoutes=require("./routes/note")
 var session = require('express-session')
 const app=express()
 // initial setup
@@ -21,12 +21,10 @@ app.use(session({
   }))
 //setup Routes
 
-app.get("/",(req,res)=>{
-  res.send("test")
-})
 
-//app.use(userRoutes)
-//app.use(noteRoutes)
+
+app.use(userRoutes)
+app.use(noteRoutes)
 
 // MongoDB setup
 mongoose.connect('mongodb+srv://dbfcis:j4MQgkB76feFW7y@cluster0.a5rkl.mongodb.net/NoteDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
