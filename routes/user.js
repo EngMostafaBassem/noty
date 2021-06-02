1af3fe1548538162c03d13fc5f768057636ae904
vip <vip@DESKTOP-TVLU5IH>
2021-06-02 22:01:03 +0200

    '22'

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const userRouter=require("express").Router()
 userRouter.get("/",userContoller.LoginPage)
 userRouter.get("/register",userContoller.RegisterPage)
 userRouter.get("/LogOut",userContoller.LogOut)
-userRouter.post("/handleSignUp",require('../validations/RegisterForm').Validations,userContoller.SaveUser)
-userRouter.post("/handleSignin",require('../validations/LoginForm').Validations,userContoller.LoginUser)
+userRouter.post("/handleSignUp",userContoller.SaveUser)
+userRouter.post("/handleSignin",userContoller.LoginUser)
 
 module.exports=userRouter
